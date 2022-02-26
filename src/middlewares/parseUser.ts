0033fe7551e7af3805da06ee169c5744e85fa2c9
Piyush <piyush41y08h@gmail.com>
2022-02-26 20:17:41 +0530

    fix parse user middleware

--- src/middlewares/parseUser.ts
+++ src/middlewares/parseUser.ts
@@ -3,8 +3,10 @@ import jwt from "jsonwebtoken";
 import { User } from "../entities/User";
 
 const parseUser: RequestHandler = async (req, res, next) => {
-  const token = req.header("Authorization").split(" ")[1];
+  const header = req.header("Authorization");
+  if (!header) return next();
 
+  const token = header.split(" ")[1];
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET) as jwt.JwtPayload;
 
